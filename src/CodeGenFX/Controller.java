f9eaa9799456b57090e8547f37c963aa3edb1b44
Felix Hillebrand <97hilfel@gmail.com>
2017-05-04 11:43:09 +0200

    Work on GUI * "MainWindow.fxml" contains "final" version of GUI (minor changes still possible) * "Controller.java" bound to "MainWindow.fxml" * Added "Main.java" try-catch so program wont fail w/o notifying developer/user

--- src/CodeGenFX/Controller.java
+++ src/CodeGenFX/Controller.java
@@ -1,20 +1,9 @@
 package CodeGenFX;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 
 public class Controller {
 	
-	@FXML
-	private Pane configPane;
-	
-	@FXML
-	private ImageView barcodePreview;
-	
-	@FXML
-	private ComboBox<IBarcode> barcodeSelector;
 	
 	public Controller(){
 	
--- src/CodeGenFX/Main.java
+++ src/CodeGenFX/Main.java
@@ -4,18 +4,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        
-        primaryStage.setTitle("CodeGen FX");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+     
+    	try {
+	      FXMLLoader sceneLoader = new FXMLLoader();
+	      
+	      Parent root = sceneLoader.load(getClass().getResource("MainWindow.fxml"));
+	  	  
+	      primaryStage.setTitle("CodeGen FX");
+	      primaryStage.setScene(new Scene(root, 300, 275));
+	      primaryStage.show();
+	      
+      }catch(Exception ex){
+	  
+	      Alert exceptionAlert = new Alert(Alert.AlertType.ERROR);
+	      exceptionAlert.setTitle("Not able to load gui");
+	      exceptionAlert.setHeaderText("A exception occurred when attempted " +
+			                             "to load \"MainWindow.fxml\" resource file");
+    		exceptionAlert.setContentText(ex.toString());
+    		exceptionAlert.showAndWait();
+      }
     }
 
 
