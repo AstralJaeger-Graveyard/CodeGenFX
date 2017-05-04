3c1d567f0a3a61a05ef04be15fd39250403d8427
Felix Hillebrand <97hilfel@gmail.com>
2017-05-04 12:30:26 +0200

    GUI work * Created for most needed obj. on the GUI a instance in the "Controller.java" class * Created for most needed obj. on the GUI a "fx:id"-property

--- src/CodeGenFX/Controller.java
+++ src/CodeGenFX/Controller.java
@@ -1,15 +1,57 @@
 package CodeGenFX;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 public class Controller {
 	
 	
-	public Controller(){
 	
+	
+	// MenuBar
+	
+	// Menu - File
+	@FXML private MenuItem newFile;
+	@FXML private MenuItem saveFile;
+	@FXML private MenuItem saveAsFile;
+	@FXML private MenuItem quitFile;
+	
+	// Menu - Edit
+	// TODO: Evaluate needed functions and controls for this menu
+	
+	// Menu - Help
+	@FXML private MenuItem about;
+	
+	
+	// SplitPane
+	
+	// Accordion
+	// TitledPane - Barcode
+	@FXML	private ComboBox<IBarcode> iBarcodeComboBox;
+	
+	// TitledPane - Configuration
+	@FXML private AnchorPane configuration;
+	
+	// TitledPane - Export Settings
+	// TODO: Evaluate needed functions and controls for this menu
+	
+	// ImageView - Barcode PreView
+	@FXML private ImageView iBarcodePreview;
+	
+	
+	/**
+	 * Contructor w/o args
+	 */
+	public Controller(){
 	
 	}
 	
+	/**
+	 * Inizialise fct for JavaFX Controller class
+	 */
 	@FXML
 	public void inizialise(){
 	
